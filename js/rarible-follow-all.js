7cf702599f727c3740c14ccfd944a36aaed0a004
Jeff Palm <jeff@jeffpalm.com>
2021-12-30 10:50:18 -0500

    added iterations prompt to rarible bookmarklet

--- js/rarible-follow-all.js
+++ js/rarible-follow-all.js
@@ -4,5 +4,16 @@
  * @Description: Follow all on rarible.com
  */
 (function () {
-    Array.from(document.querySelectorAll('button')).filter((el) => el.innerText == 'Follow').forEach((el) => el.click());
+    let times = prompt('# iterations', '1');
+    let count = 0;
+    let loop = () => {
+        Array.from(document.querySelectorAll('button')).filter((el) => el.innerText == 'Follow').forEach((el) => el.click());
+        let el = document.getElementsByClassName('ReactVirtualized__Grid ReactVirtualized__List')[0];
+        el.scrollTop = el.scrollHeight;
+        count++;
+        if (count < 5) {
+            setTimeout(loop, 1000);
+        }
+    };
+    loop();
 })();
