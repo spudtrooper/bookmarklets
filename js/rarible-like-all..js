35e887b0ed21f8642032e3cc9735a27e0eff073e
Jeff Palm <jeff@jeffpalm.com>
2021-12-31 09:52:48 -0500

    update Fri Dec 31 09:52:48 EST 2021

--- js/rarible-like-all..js
+++ js/rarible-like-all..js
@@ -8,7 +8,7 @@
     let count = 0;
     let loop = () => {
         Array.from(document.getElementsByClassName('sc-bdvvtL sc-gKclnd sc-iCfMLu sc-cjrPHo kMXwoE eehqFj cCtwBW sc-cvlWTT dVFwth')).forEach((el) => el.click());
-        window.scrollTo(0,document.body.scrollHeight);
+        window.scrollTo(0, document.body.scrollHeight);
         if (++count < times) {
             setTimeout(loop, 3000);
         }
