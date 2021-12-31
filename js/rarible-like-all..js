7b64a9ec81255d223734b0088a331a84bdbca68c
Jeff Palm <jeff@jeffpalm.com>
2021-12-31 10:03:48 -0500

    update Fri Dec 31 10:03:48 EST 2021

--- js/rarible-like-all..js
+++ js/rarible-like-all..js
@@ -4,17 +4,11 @@
  * @Description: Like all on rarible.com
  */
 (function () {
-    let times = parseInt(prompt('# iterations', '1'));
-    let count = 0;
     let loop = () => {
         Array.from(document.getElementsByClassName('sc-bdvvtL sc-gKclnd sc-iCfMLu sc-cjrPHo kMXwoE eehqFj cCtwBW sc-cvlWTT dVFwth')).forEach((el) => el.click());
-        window.scrollTo(0, document.body.scrollHeight);
-        setTimeout(function () {
-            Array.from(document.querySelectorAll('button')).filter((el) => el.innerText == 'Load more').forEach((el) => el.click());
-            if (++count < times) {
-                setTimeout(loop, 3000);
-            }
-        }, 2000);
+        window.scrollTo(0, 2 * document.body.scrollHeight / 3);
+        Array.from(document.querySelectorAll('button')).filter((el) => el.innerText == 'Load more').forEach((el) => el.click());
+        setTimeout(loop, 1000);
     };
     loop();
 })();
