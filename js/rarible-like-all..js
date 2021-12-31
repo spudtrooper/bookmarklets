005c9b988026e82922b01cd7ac14eb2c6add87c6
Jeff Palm <jeff@jeffpalm.com>
2021-12-31 09:52:33 -0500

    update Fri Dec 31 09:52:33 EST 2021

---
+++ js/rarible-like-all..js
@@ -0,0 +1,17 @@
+
+/*
+ * @Title: Rarible Like All
+ * @Description: Like all on rarible.com
+ */
+(function () {
+    let times = parseInt(prompt('# iterations', '1'));
+    let count = 0;
+    let loop = () => {
+        Array.from(document.getElementsByClassName('sc-bdvvtL sc-gKclnd sc-iCfMLu sc-cjrPHo kMXwoE eehqFj cCtwBW sc-cvlWTT dVFwth')).forEach((el) => el.click());
+        window.scrollTo(0,document.body.scrollHeight);
+        if (++count < times) {
+            setTimeout(loop, 3000);
+        }
+    };
+    loop();
+})();
