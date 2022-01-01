5fccd793a2bf1678056e7774e9a0c73ad055e4d8
Jeff Palm <jeff@jeffpalm.com>
2022-01-01 16:46:22 -0500

    update Sat Jan  1 16:46:22 EST 2022

--- js/metamask-sign-auto.js
+++ js/metamask-sign-auto.js
@@ -4,11 +4,13 @@
  * @Description: Automatically signs trivial metamask signature requests
  */
 (function () {
+    let clicks = 0;
     let clickSign = () => {
         let div = document.getElementsByClassName('request-signature__rows')[0];
         if (!div) return;
         if (!div.innerText.includes('I would like to follow user')) return;
         Array.from(document.getElementsByClassName('button')).filter((el) => el.getAttribute('data-testid') && el.getAttribute('data-testid') == 'request-signature__sign').forEach((el) => el.click());
+        console.log('clicks:' + (++clicks));
     };
     setInterval(clickSign, 300);
 })();
