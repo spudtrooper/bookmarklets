f7ab7b824c8560ac9e9fe8a68810478f75007343
Jeff Palm <jeff@jeffpalm.com>
2022-01-02 17:37:50 -0500

    update Sun Jan  2 17:37:50 EST 2022

--- js/metamask-sign-auto.js
+++ js/metamask-sign-auto.js
@@ -5,12 +5,20 @@
  */
 (function () {
     let clicks = 0;
+    const sleep = 150;
+    const longSleep = 3000;
     let clickSign = () => {
         let div = document.getElementsByClassName('request-signature__rows')[0];
         if (!div) return;
         if (!div.innerText.includes('I would like to follow user')) return;
         Array.from(document.getElementsByClassName('button')).filter((el) => el.getAttribute('data-testid') && el.getAttribute('data-testid') == 'request-signature__sign').forEach((el) => el.click());
         console.log('clicks:' + (++clicks));
+        if (clicks % 50 == 0) {
+            console.log('sleeping...');
+            setTimeout(clickSign, longSleep);
+        } else {
+            setTimeout(clickSign, sleep);
+        }
     };
-    setInterval(clickSign, 300);
+    setTimeout(clickSign, 5000);
 })();
