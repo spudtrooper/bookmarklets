a666f980f5c881b1ddc3ca128755e0cbc8050610
Jeff Palm <jeff@jeffpalm.com>
2022-02-02 07:53:24 -0500

    update Wed Feb  2 07:53:24 EST 2022

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,7 +7,7 @@ set -e
 mkdir -p output
 args=(
     --base_source_url
-    https://github.com/spudtrooper/bookmarklets/blob/main/js
+    https://github.com/spudtrooper/bookmarklets/blob/main/src
     --js_dir
     src
     --outfile_html
