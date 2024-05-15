c3d414e3ff44d4f82b233975b7ddac521f51b804
Jeff Palm <jeffpalm@gmail.com>
2024-05-15 06:33:11 -0400

    update Wed May 15 06:33:11 EDT 2024

--- src/instagram-follow-all.js
+++ src/instagram-follow-all.js
@@ -2,67 +2,67 @@
  * @Description: Follows people in bulk, 25 at a time.
  */
 (function () {
-    function clickFollowersButton() {
-        Array.from(document.getElementsByTagName('a')).forEach((el) => {
-            if (el.href && el.href.match(/\/followers\//)) {
-                console.log('click');
-                el.click();
-            }
-        });
-    }
+  function clickFollowersButton() {
+    Array.from(document.getElementsByTagName('a')).forEach((el) => {
+      if (el.href && el.href.match(/\/followers\//)) {
+        console.log('click');
+        el.click();
+      }
+    });
+  }
 
-    function findFollowButtons() {
-        return Array.from(document.querySelectorAll('button')).filter(el => el.textContent == 'Follow');
-    }
+  function findFollowButtons() {
+    return Array.from(document.querySelectorAll('button')).filter(el => el.textContent == 'Follow');
+  }
 
-    function findFollowButton() {
-        return Array.from(document.querySelectorAll('button')).find(el => el.textContent == 'Follow');
-    }
+  function findFollowButton() {
+    return Array.from(document.querySelectorAll('button')).find(el => el.textContent == 'Follow');
+  }
 
-    function expandDialog() {
-        const div = Array.from(document.querySelectorAll('div')).filter(el => el.style?.maxHeight === "400px")[0];
-        if (!div) return;
-        div.style.maxHeight = "1800px";
-    }
+  function expandDialog() {
+    const div = Array.from(document.querySelectorAll('div')).filter(el => el.style?.maxHeight === "400px")[0];
+    if (!div) return;
+    div.style.maxHeight = "1800px";
+  }
 
-    function click(btn) {
-        btn.style.backgroundColor = "green";
-        btn.click();
-    }
+  function click(btn) {
+    btn.style.backgroundColor = "green";
+    btn.click();
+  }
 
-    function main() {
-        clickFollowersButton();
+  function main() {
+    clickFollowersButton();
 
-        expandDialog();
+    expandDialog();
 
-        const loop = () => {
-            const followButtons = findFollowButtons();
-            if (!followButtons || !followButtons.length || followButtons.length < 5) {
-                console.log('waiting for follow buttons');
-                setTimeout(loop, 1000);
-                return;
-            }
-            const followNext = (done) => {
-                let btn = findFollowButton();
-                if (!btn) {
-                    setTimeout(followNext.bind(null, done), 200);
-                    return;
-                }
-                click(btn);
-                done.call(null);
-            };
-            const burnDownFollowingButtons = () => {
-                if (followButtons.length) {
-                    let nextBtn = followButtons.pop(0);
-                    nextBtn.click();
-                    followNext(burnDownFollowingButtons);
-                }
+    const loop = () => {
+      const followButtons = findFollowButtons();
+      if (!followButtons || !followButtons.length || followButtons.length < 5) {
+        console.log('waiting for follow buttons');
+        setTimeout(loop, 1000);
+        return;
+      }
+      const followNext = (done) => {
+        let btn = findFollowButton();
+        if (!btn) {
+          setTimeout(followNext.bind(null, done), 200);
+          return;
+        }
+        click(btn);
+        done.call(null);
+      };
+      const burnDownFollowingButtons = () => {
+        if (followButtons.length) {
+          let nextBtn = followButtons.pop(0);
+          nextBtn.click();
+          followNext(burnDownFollowingButtons);
+        }
 
-            };
-            burnDownFollowingButtons();
-        };
-        loop();
-    }
+      };
+      burnDownFollowingButtons();
+    };
+    loop();
+  }
 
-    main();
+  main();
 })();
--- src/instagram-unfollow-all.js
+++ src/instagram-unfollow-all.js
@@ -2,54 +2,54 @@
  * @Description: Unfollows people in bulk, 25 at a time.
  */
 (function () {
-    function clickFollowingButton() {
-        Array.from(document.getElementsByTagName('a')).forEach((el) => {
-            if (el.href && el.href.match(/\/following\//)) {
-                console.log('click');
-                el.click();
-            }
-        });
-    }
+  function clickFollowingButton() {
+    Array.from(document.getElementsByTagName('a')).forEach((el) => {
+      if (el.href && el.href.match(/\/following\//)) {
+        console.log('click');
+        el.click();
+      }
+    });
+  }
 
-    function findFollowingButtons() {
-        return Array.from(document.querySelectorAll('button')).filter(el => el.textContent == 'Following');
-    }
+  function findFollowingButtons() {
+    return Array.from(document.querySelectorAll('button')).filter(el => el.textContent == 'Following');
+  }
 
-    function findUnfollowButton() {
-        return Array.from(document.querySelectorAll('button')).find(el => el.textContent == 'Unfollow');
-    }
+  function findUnfollowButton() {
+    return Array.from(document.querySelectorAll('button')).find(el => el.textContent == 'Unfollow');
+  }
 
-    function main() {
-        clickFollowingButton();
+  function main() {
+    clickFollowingButton();
 
-        let loop = () => {
-            let followingButtons = findFollowingButtons();
-            if (!followingButtons || !followingButtons.length || followingButtons.length < 5) {
-                console.log('waiting for following buttons');
-                setTimeout(loop, 1000);
-                return;
-            }
-            let unfollowNext = (done) => {
-                let btn = findUnfollowButton();
-                if (!btn) {
-                    setTimeout(unfollowNext.bind(null, done), 200);
-                    return;
-                }
-                btn.click();
-                done.call(null);
-            };
-            let burnDownFollowingButtons = () => {
-                if (followingButtons.length) {
-                    let nextBtn = followingButtons.pop(0);
-                    nextBtn.click();
-                    unfollowNext(burnDownFollowingButtons);
-                }
+    let loop = () => {
+      let followingButtons = findFollowingButtons();
+      if (!followingButtons || !followingButtons.length || followingButtons.length < 5) {
+        console.log('waiting for following buttons');
+        setTimeout(loop, 1000);
+        return;
+      }
+      let unfollowNext = (done) => {
+        let btn = findUnfollowButton();
+        if (!btn) {
+          setTimeout(unfollowNext.bind(null, done), 200);
+          return;
+        }
+        btn.click();
+        done.call(null);
+      };
+      let burnDownFollowingButtons = () => {
+        if (followingButtons.length) {
+          let nextBtn = followingButtons.pop(0);
+          nextBtn.click();
+          unfollowNext(burnDownFollowingButtons);
+        }
 
-            };
-            burnDownFollowingButtons();
-        };
-        loop();
-    }
+      };
+      burnDownFollowingButtons();
+    };
+    loop();
+  }
 
-    main();
+  main();
 })();
